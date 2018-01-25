6aa1f1180cfb89e1a39420d2cec57cd9a3e3f0d1
104062212 <karentseng1027@gmail.com>
2018-01-25 15:39:12 +0800

    can create mutiple items

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -134,27 +134,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    //Rectangle
-    QBrush blueBrush(Qt::blue);
-    QPen blackpen(Qt::black);
-    blackpen.setWidth(6);
-    unit *a = new unit();
-    a->length = ui->lineEdit->text().toInt();
-    a->width = ui->lineEdit_2->text().toInt();
-    a->xi = ui->lineEdit_3->text().toInt();
-    a->yi = ui->lineEdit_4->text().toInt();
-    a->type = ui->comboBox->currentText();
-    if(ui->comboBox->currentIndex() == 0){
-        a->color = Qt::yellow;
-    } else if (ui->comboBox->currentIndex() == 1){
-        a->color = Qt::blue;
-    } else {
-        a->color = Qt::red;
-    }
-    ui->view->scene()->addItem(a);
-}
+//void MainWindow::on_pushButton_clicked()
+//{
+//    //Rectangle
+//    QBrush blueBrush(Qt::blue);
+//    QPen blackpen(Qt::black);
+//    blackpen.setWidth(6);
+//    unit *a = new unit();
+
+//    // moving
+//    unit *move = new unit();
+//    move->length = ui->move_length->text().toInt();
+//    move->width = ui->move_width->text().toInt();
+//    move->xi = 1;
+//    move->yi = 2;
+////    move->type = ui->comboBox->currentText();
+////    a->length = ui->lineEdit->text().toInt();
+////    a->width = ui->lineEdit_2->text().toInt();
+////    a->xi = ui->lineEdit_3->text().toInt();
+////    a->yi = ui->lineEdit_4->text().toInt();
+////    a->type = ui->comboBox->currentText();
+
+////    if(ui->comboBox->currentIndex() == 0){
+////        a->color = Qt::yellow;
+////    } else if (ui->comboBox->currentIndex() == 1){
+////        a->color = Qt::blue;
+////    } else {
+////        a->color = Qt::red;
+////    }
+//    ui->view->scene()->addItem(a);
+//}
 
 void MainWindow::on_toolButton_clicked()
 {
@@ -163,5 +172,123 @@ void MainWindow::on_toolButton_clicked()
         ui->toolButton->setStyleSheet("background-color: rgba(122,122,122,122); border:none;");
     } else {
         ui->toolButton->setStyleSheet("");
+    }
+}
+
+void MainWindow::on_move_create_clicked()
+{
+    QBrush blueBrush(Qt::blue);
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(6);
+
+    int number = ui->move_num->value();
+    int position = 5;
+    while(number--){
+        unit *move = new unit();
+        move->length = ui->move_length->text().toInt();
+        move->width = ui->move_width->text().toInt();
+        move->xi = position;
+        move->yi = 5;
+
+        move->color = Qt::yellow;
+
+        ui->view->scene()->addItem(move);
+
+        position += ui->move_length->text().toInt() + 3;
+    }
+}
+
+
+void MainWindow::on_dispenser_create_clicked()
+{
+    QBrush blueBrush(Qt::blue);
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(6);
+
+    int number = ui->dispenser_num->value();
+    int position = 5;
+    while(number--){
+        unit *dispenser = new unit();
+        dispenser->length = ui->dispenser_length->text().toInt();
+        dispenser->width = ui->dispenser_width->text().toInt();
+        dispenser->xi = position;
+        dispenser->yi = 10;
+
+        dispenser->color = Qt::red;
+
+        ui->view->scene()->addItem(dispenser);
+
+        position += ui->dispenser_length->text().toInt() + 3;
+    }
+}
+
+void MainWindow::on_merge_create_clicked()
+{
+    QBrush blueBrush(Qt::blue);
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(6);
+
+    int number = ui->merge_num->value();
+    int position = 5;
+    while(number--){
+        unit *merge = new unit();
+        merge->length = ui->merge_length->text().toInt();
+        merge->width = ui->merge_width->text().toInt();
+        merge->xi = position;
+        merge->yi = 15;
+
+        merge->color = Qt::blue;
+
+        ui->view->scene()->addItem(merge);
+
+        position += ui->merge_length->text().toInt() + 3;
+    }
+}
+
+void MainWindow::on_cycling_create_clicked()
+{
+    QBrush blueBrush(Qt::blue);
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(6);
+
+    int number = ui->cycling_num->value();
+    int position = 5;
+    while(number--){
+        unit *cycling = new unit();
+        cycling->length = ui->cycling_length->text().toInt();
+        cycling->width = ui->cycling_width->text().toInt();
+        cycling->xi = position;
+        cycling->yi = 20;
+
+        cycling->color = Qt::green;
+
+        ui->view->scene()->addItem(cycling);
+
+        position += ui->cycling_length->text().toInt() + 3;
+
+    }
+
+}
+
+void MainWindow::on_heater_create_clicked()
+{
+    QBrush blueBrush(Qt::blue);
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(6);
+
+    int number = ui->heater_num->text().toInt();
+    int position = 5;
+    while(number--){
+        unit *heater = new unit();
+        heater->length = 5;
+        heater->width = 5;
+        heater->xi = position;
+        heater->yi = 25;
+
+        heater->color = QColor(100, 100, 150);
+
+        ui->view->scene()->addItem(heater);
+
+        position += 8;
     }
 }
